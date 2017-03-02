31f71d7531fc54bae10103b3758cf63a1321971a
Alec Matthews <alec500oo@gmail.com>
2017-03-01 22:04:16 -0800

    Changed the whole thing... it works now with wifi functions. I need to make the server portion of it and relate it to the matrix

--- Esp8266_led_matrix_controller/Esp8266_led_matrix_controller.ino
+++ Esp8266_led_matrix_controller/Esp8266_led_matrix_controller.ino
@@ -10,6 +10,8 @@
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
 
+
+WiFiServer server(80);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -17,32 +19,42 @@ void setup() {
   pinMode(12, OUTPUT); // Blue LED
   pinMode(14, OUTPUT); // Green LED
 
+  digitalWrite(12, LOW);
   digitalWrite(14, HIGH);
+
+  Serial.println(WiFi.softAPIP());
 }
 
 bool wifiEnabled = false;
-bool buttonPressed = false;
-long waitTime = 0;
+bool networkActive = false;
+long activeTime = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(16)) {
-    buttonPressed = true;
-    waitTime = millis();
-  } else {
-    buttonPressed = false;
-  }
-  while(buttonPressed) {
-    if((millis() - waitTime) >= 10000) {
-      Serial.println("WiFi Activating");
-      digitalWrite(14, LOW);
-      digitalWrite(12, HIGH);
+  if (networkActive) {
+    Serial.println("Creating Network");
+    WiFi.softAP("LEDCONTROLLER");
+    Serial.print("Host At: ");
+    Serial.println(WiFi.softAPIP());
 
-      Serial.println("Setting Up Soft AP ....");
-      Serial.println(WiFi.softAP("LEDBADGE") ? "Ready" : "Failed"); 
+    while (networkActive) {
+      yield(); // this needs to be here so it does not crash!!!!
+      if (WiFi.softAPgetStationNum() < 1 && (millis() - activeTime) > 30000) {
+        Serial.println("No Acctivity... Turning off network");
+        Serial.println(WiFi.softAPdisconnect(true) ? "Sucess" : "Fail");
+        networkActive = false;
+        digitalWrite(12, LOW);
+        digitalWrite(14, HIGH);
+      } else {
+        Serial.print("Number of devices Connected: ");
+        Serial.println(WiFi.softAPgetStationNum());
+        delay(2000);
+      }
     }
-
-    Serial.printf("Soft Ap number of connections = %d\n", WiFi.softAPgetStationNum());
-    delay(3000);
   }
 
+  if (digitalRead(16) == HIGH) {
+    networkActive = true;
+    activeTime = millis();
+    digitalWrite(12, HIGH);
+    digitalWrite(14, LOW);
+  }
 }
