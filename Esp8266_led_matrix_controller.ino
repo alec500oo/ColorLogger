dcbae4f48cf9dd447209215f9f945d680baddd02
Alec Matthews <alec500oo@gmail.com>
2017-03-01 16:09:17 -0800

    second commit

--- Esp8266_led_matrix_controller.ino
+++ Esp8266_led_matrix_controller.ino
@@ -1,8 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(16, INPUT);
+
 }
 
+bool wifiEnabled = false;
 void loop() {
   // put your main code here, to run repeatedly:
 
