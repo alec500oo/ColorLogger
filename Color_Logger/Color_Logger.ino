4e0c9b06367e12e6615d0465b3229fd586646585
Alec Matthews <alec500oo@gmail.com>
2017-04-01 15:47:19 -0700

    changed how the wifi is handled

--- Color_Logger/Color_Logger.ino
+++ Color_Logger/Color_Logger.ino
@@ -1,3 +1,5 @@
+#include <SD.h>
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiAP.h>
 #include <ESP8266WiFiGeneric.h>
@@ -9,6 +11,7 @@
 #include <WiFiClientSecure.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
+
 #include <Wire.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
@@ -19,96 +22,95 @@
 #define GREENLED 15
 #define BLUELED 2
 
+enum state {
+  NORMAL,
+  NETWORKACTIVE,
+  CLIENTCONNECTED
+};
+
 WiFiServer server(23);
+WiFiClient client;
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
 void setup() {
   // put your setup code here, to run once:
+  //Setup serial and the led
   Serial.begin(115200);
-  pinMode(16, INPUT);
-  pinMode(12, OUTPUT); // Blue LED
-  pinMode(14, OUTPUT); // Green LED
+  pinMode(BUTTON, INPUT);
+  pinMode(BLUELED, OUTPUT);
+  pinMode(GREENLED, OUTPUT);
 
-  digitalWrite(12, LOW);
-  digitalWrite(14, HIGH);
+  digitalWrite(BLUELED, LOW);
+  digitalWrite(GREENLED, HIGH);
 
-  matrix.begin(0x70);
-  matrix.setTextWrap(false);
-  matrix.setTextSize(1);
-  matrix.setTextColor(LED_GREEN);
-  matrix.clear();
-  matrix.writeDisplay();
-  
+  //start the server for changing settings
   server.begin();
 }
 
-bool networkActive = false;
+enum state networkState = NORMAL;
 long activeTime = 0;
 
-String matrixText = "Stealhead 8176";
 void loop() {
-  if (networkActive) {
-    Serial.println("Creating Network");
-    WiFi.softAP("LEDCONTROLLER");
-    Serial.print("Host At: ");
-    Serial.println(WiFi.softAPIP());
-
-    while (networkActive) {
-      yield(); // this needs to be here so it does not crash!!!!
+  switch (networkState) {
+    case NETWORKACTIVE:
+      //Check for network actifity, if none stop the network
       if (WiFi.softAPgetStationNum() < 1 && (millis() - activeTime) > 30000) {
         Serial.print("No Acctivity... Turning off network... ");
-        Serial.println(WiFi.softAPdisconnect(true) ? "Sucess" : "Fail");
-        networkActive = false;
-        digitalWrite(12, LOW);
-        digitalWrite(14, HIGH);
+        Serial.println(WiFi.softAPdisconnect(true) ? "Sucess" : "Failiure");
+        digitalWrite(BLUELED, LOW);
+        digitalWrite(GREENLED, HIGH);
+        networkState = NORMAL;
       } else {
-        WiFiClient client = server.available();
-
+        //get a new client
+        client = server.available();
         if (client) {
-          Serial.println("New Client Connected");
-
-          while (client.connected()) {
-            yield();
-
-            if (client.available()) {
-              String req = client.readStringUntil('\r');
-              Serial.println(req);
-              client.flush();
-
-              if (req.indexOf("/ChangeString/") != -1) {
-                matrixText = req.substring(14);
-
-                Serial.print("Requested String... ");
-                Serial.println(matrixText);
-              } else if (req.indexOf("/Close/") != -1) {
-                client.stop();
-              } else {
-                Serial.println("Invalid Request");
-              }
-
-              client.flush();
-              client.print("The result has been logged");
-              delay(1);
-            }
-          }
+          Serial.print("New Client Connected... ");
+          Serial.println("Waiting for a command");
+          client.println("Waiting for a command");
+          networkState = CLIENTCONNECTED;
         }
       }
-    }
-  }
+      break;
+    case CLIENTCONNECTED:
+      if (client.connected()) {
+        if (client.available()) {
+          String req = client.readStringUntil('\r');
+          Serial.println(req);
+          client.flush();
 
-  if (digitalRead(16) == HIGH) {
-    networkActive = true;
-    activeTime = millis();
-    digitalWrite(12, HIGH);
-    digitalWrite(14, LOW);
-  }
+          if (req.indexOf("/ChangeString/") != -1) {
 
-  int stringLength = (matrixText.length()*7);
-  for (int8_t x = 7; x >= -stringLength; x--) {
-    matrix.clear();
-    matrix.setCursor(x, 0);
-    matrix.print(matrixText);
-    matrix.writeDisplay();
-    delay(100);
+          } else if (req.indexOf("/Close/") != -1) {
+            client.println("CLOSING CONNECTION");
+            delay(100);
+            client.stop();
+            Serial.print("WiFi AP turned off... ");
+            Serial.println(WiFi.softAPdisconnect(true) ? "Success" : "Failiure");
+            digitalWrite(BLUELED, LOW);
+            digitalWrite(GREENLED, HIGH);
+            networkState = NORMAL;
+          } else {
+            client.println("Invalid Request");
+            Serial.println("Invalid Request");
+          }
+        }
+      } else {
+        networkState = NORMAL;
+      }
+      break;
+    case NORMAL:
+      if (digitalRead(BUTTON) == LOW) {
+        activeTime = millis();
+        //switch the led state
+        digitalWrite(BLUELED, HIGH);
+        digitalWrite(GREENLED, LOW);
+        //create the local wifi network
+        Serial.println("Creating Network");
+        WiFi.softAP("LEDCONTROLLER");
+        Serial.print("Host At: ");
+        Serial.println(WiFi.softAPIP());
+        networkState = NETWORKACTIVE;
+      }
+      break;
   }
 }
